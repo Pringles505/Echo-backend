d09cd775045119955307a7d9cf62635c1a7d0ecc
gdelalastra <gonzalo.delalastra.ceb@immune.institute>
2025-06-25 13:26:36 +0200

    update socket connection for updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
@@ -6,7 +7,21 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { customAlphabet } = require('nanoid');
-require('dotenv').config();
+const path = require('path');
+const fs = require('fs');
+
+// Helper to save image and return URL
+async function saveProfilePicture(base64Image, userId) {
+  const uploadDir = path.join(__dirname, 'uploads');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  const filename = `${userId}-${Date.now()}.png`;
+  const filePath = path.join(uploadDir, filename);
+  const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
+  fs.writeFileSync(filePath, base64Data, { encoding: 'base64' });
+  return `/uploads/${filename}`;
+}
 
 const userSocketMap = {};
 
@@ -15,17 +30,19 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: ['http://localhost:5173', 'https://chat-tuah-frontend.vercel.app'],
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT'],
+    credentials: true,
   },
 });
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://chat-tuah-frontend.vercel.app'],
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'PUT'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
 }));
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const mongoUri = process.env.MONGO_URI;
 
@@ -44,6 +61,8 @@ const userSchema = new mongoose.Schema({
   publicIdentityKeyEd25519: String,
   signedPreKey: String,
   signature: String,
+  aboutme: { type: String, default: '' },
+  profilePicture: { type: String, default: '' },
 });
 
 const messageSchema = new mongoose.Schema({
@@ -67,17 +86,14 @@ const authenticate = (socket, next) => {
 
   if (!token) {
     console.warn('No token provided, allowing unauthenticated access for login/register');
-
     return next();
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-
     if (!err && decoded) {
       userSocketMap[decoded.id] = socket.id;
       console.log(`User ${decoded.username} is mapped to socket ${socket.id}`);
     }
-
     if (err) {
       console.error('Invalid token:', err);
       return next(new Error('Authentication error'));
@@ -141,64 +157,63 @@ io.on('connection', (socket) => {
     } catch (err) {
         console.error('Error fetching messages:', err);
     }
-});
+  });
 
-// Get the SignedPreKey Array (PreyKey + Signature) for XEdDSA 
-socket.on('getSignedPreKey', async ({ targetUserId }, callback) => {
-  console.log(`🔍 Fetching SignedPreKey for user: ${targetUserId}`);
-  try {
-    const user = await User.findOne({ id: targetUserId });
-    if (!user) {
-      console.error('❌ User not found');
-      return callback({ success: false, error: 'User not found' });
+  // Get the SignedPreKey Array (PreyKey + Signature) for XEdDSA 
+  socket.on('getSignedPreKey', async ({ targetUserId }, callback) => {
+    console.log(`🔍 Fetching SignedPreKey for user: ${targetUserId}`);
+    try {
+      const user = await User.findOne({ id: targetUserId });
+      if (!user) {
+        console.error('❌ User not found');
+        return callback({ success: false, error: 'User not found' });
+      }
+      console.log('✅ Found PreKey:', user.signedPreKey);
+      console.log('✅ Found Signature:', user.signature);
+      
+      // Return the SignedPreKey as an array [publicPreKey, signature]
+      callback({ success: true, signedPreKey: user.signedPreKey, signature: user.signature });
+    } catch (error) {
+      console.error('❌ Error fetching SignedPreKey:', error);
+      callback({ success: false, error: 'Internal server error' });
     }
-    console.log('✅ Found PreKey:', user.signedPreKey);
-    console.log('✅ Found Signature:', user.signature);
-    
-    // Return the SignedPreKey as an array [publicPreKey, signature]
-    callback({ success: true, signedPreKey: user.signedPreKey, signature: user.signature });
-  } catch (error) {
-    console.error('❌ Error fetching SignedPreKey:', error);
-    callback({ success: false, error: 'Internal server error' });
-  }
-});
+  });
 
-// Get publicIdentityKey in Montgomery format
-socket.on('getPublicIdentityKeyX25519', async ({ targetUserId }, callback) => {
-  console.log(`🔍 Fetching PublicIdentityKeyX25519 for user: ${targetUserId}`);
-  try {
-    const user = await User.findOne({ id: targetUserId });
-    if (!user) {
-      console.error('❌ User not found');
-      return callback({ success: false, error: 'User not found' });
+  // Get publicIdentityKey in Montgomery format
+  socket.on('getPublicIdentityKeyX25519', async ({ targetUserId }, callback) => {
+    console.log(`🔍 Fetching PublicIdentityKeyX25519 for user: ${targetUserId}`);
+    try {
+      const user = await User.findOne({ id: targetUserId });
+      if (!user) {
+        console.error('❌ User not found');
+        return callback({ success: false, error: 'User not found' });
+      }
+      console.log('✅ Found publicIdentityKeyX25519:', user.publicIdentityKeyX25519);
+      callback({ success: true, publicIdentityKeyX25519: user.publicIdentityKeyX25519 });
+    } catch (error) {
+      console.error('❌ Error fetching publicIdentityKeyX25519:', error);
+      callback({ success: false, error: 'Internal server error' });
     }
-    console.log('✅ Found publicIdentityKeyX25519:', user.publicIdentityKeyX25519);
-    callback({ success: true, publicIdentityKeyX25519: user.publicIdentityKeyX25519 });
-  } catch (error) {
-    console.error('❌ Error fetching publicIdentityKeyX25519:', error);
-    callback({ success: false, error: 'Internal server error' });
-  }
-});
+  });
 
-// Get publicIdentityKey in Edwards format
-socket.on('getPublicIdentityKeyEd25519', async ({ targetUserId }, callback) => {
-  console.log(`🔍 Fetching publicIdentityKeyEd25519 for user: ${targetUserId}`);
-  try {
-    const user = await User.findOne({ id: targetUserId });
-    if (!user) {
-      console.error('❌ User not found');
-      return callback({ success: false, error: 'User not found' });
+  // Get publicIdentityKey in Edwards format
+  socket.on('getPublicIdentityKeyEd25519', async ({ targetUserId }, callback) => {
+    console.log(`🔍 Fetching publicIdentityKeyEd25519 for user: ${targetUserId}`);
+    try {
+      const user = await User.findOne({ id: targetUserId });
+      if (!user) {
+        console.error('❌ User not found');
+        return callback({ success: false, error: 'User not found' });
+      }
+      console.log('✅ Found publicIdentityKeyEd25519:', user.publicIdentityKeyEd25519);
+      callback({ success: true, publicIdentityKeyEd25519: user.publicIdentityKeyEd25519 });
+    } catch (error) {
+      console.error('❌ Error fetching publicIdentityKeyEd25519:', error);
+      callback({ success: false, error: 'Internal server error' });
     }
-    console.log('✅ Found publicIdentityKeyEd25519:', user.publicIdentityKeyEd25519);
-    callback({ success: true, publicIdentityKeyEd25519: user.publicIdentityKeyEd25519 });
-  } catch (error) {
-    console.error('❌ Error fetching publicIdentityKeyEd25519:', error);
-    callback({ success: false, error: 'Internal server error' });
-  }
-});
+  });
 
-
-socket.on('disconnect', () => {
+  socket.on('disconnect', () => {
     console.log(`🔴User with socket ID ${socket.id} disconnected.🔴`);
 
     for (const userId in userSocketMap) {
@@ -210,7 +225,7 @@ socket.on('disconnect', () => {
   });
 
   socket.on('register', async (data, callback) => {
-    const { username, password, keyBundle } = data;
+    const { username, password, keyBundle, aboutme, profilePicture } = data;
     const { publicIdentityKeyX25519, publicIdentityKeyEd25519, publicSignedPreKey } = keyBundle;
     const [signedPreKey, signature] = publicSignedPreKey;
 
@@ -222,11 +237,17 @@ socket.on('disconnect', () => {
       const hashedPassword = await bcrypt.hash(password, 10);
       const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 5);
       const id = nanoid();
-      const user = new User({ id, username, hashedPassword, 
-        publicIdentityKeyX25519: publicIdentityKeyX25519, 
-        publicIdentityKeyEd25519: publicIdentityKeyEd25519,
-        signedPreKey: signedPreKey,
-        signature: signature,});
+      const user = new User({
+        id,
+        username,
+        hashedPassword,
+        publicIdentityKeyX25519,
+        publicIdentityKeyEd25519,
+        signedPreKey,
+        signature,
+        aboutme: typeof aboutme === 'string' ? aboutme : '',
+        profilePicture: typeof profilePicture === 'string' ? profilePicture : '',
+      });
 
       await user.save();
       console.log('User saved:', user);
@@ -313,7 +334,7 @@ socket.on('disconnect', () => {
         console.error('Error checking messages:', err);
         callback({ success: false, error: 'Internal server error' });
     }
-});
+  });
 
   socket.on('getLatestMessageNumber', async (data, callback) => {
     const { userId, targetUserId } = data;
@@ -401,13 +422,86 @@ socket.on('disconnect', () => {
       console.log('Found user:', user);
       callback({ success: true, user: { id: user.id, username: user.username } });
     } catch (error) {
-      console.error('Error searching for user:', error);
+      console.error('Error searching user:', error);
       callback({ success: false, error: 'Internal server error' });
     }
   });
+
+  socket.on('updateUserInfo', async (data, callback) => {
+    console.log('========== [updateUserInfo] ==========');does
+    console.log('Received data:', JSON.stringify(data, null, 2));
+    const { userId, username, aboutme, profilePicture, oldPassword, newPassword } = data;
+    try {
+      const user = await User.findOne({ id: userId });
+      if (!user) {
+        return callback && callback({ success: false, error: 'User not found' });
+      }
+
+      // Username update
+      if (typeof username === 'string' && username.length > 0) {
+        user.username = username;
+      }
+
+      // About me update
+      if (typeof aboutme === 'string') {
+        user.aboutme = aboutme;
+      }
+
+      // Profile picture update (expects base64 string from frontend)
+      if (typeof profilePicture === 'string' && profilePicture.startsWith('data:image/')) {
+        const url = await saveProfilePicture(profilePicture, userId);
+        user.profilePicture = url;
+      }
+
+      // Password update
+      if (oldPassword && newPassword) {
+        const isMatch = await bcrypt.compare(oldPassword, user.hashedPassword);
+        if (!isMatch) {
+          return callback && callback({ success: false, error: 'Old password is incorrect' });
+        }
+        user.hashedPassword = await bcrypt.hash(newPassword, 10);
+      }
+
+      await user.save();
+      callback && callback({
+        success: true,
+        user: {
+          id: user.id,
+          username: user.username,
+          aboutme: user.aboutme,
+          profilePicture: user.profilePicture,
+        }
+      });
+    } catch (err) {
+      console.error('Error updating user info:', err);
+      callback && callback({ success: false, error: 'Internal server error' });
+    }
+  });
+
+  socket.on('getUserInfo', async ({ userId }, cb) => {
+    try {
+      const user = await User.findOne({ id: userId });
+      if (user) {
+        console.log('User found:', user);
+        cb({ 
+          success: true, 
+          user: {
+            id: user.id,
+            username: user.username,
+            aboutme: user.aboutme,
+            profilePicture: user.profilePicture,
+            friends: user.friends
+          }
+        });
+      } else {
+        cb({ success: false, error: 'User not found' });
+      }
+    } catch (err) {
+      console.error('Error fetching user info:', err);
+      cb({ success: false, error: 'Internal server error' });
+    }
+  });
 });
-
-
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
